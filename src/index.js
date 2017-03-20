7823f65b9a5a19425b514f7e8cb5e9e39e551146
纳兰天忆 <103520146@qq.com>
2017-03-20 10:51:30 +0800

    User构建

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import dva from 'dva';
 import './index.css';
+import createLoading from 'dva-loading';
+import { browserHistory } from 'dva/router';
+import { message } from 'antd';
 
-// 1. Initialize
-const app = dva();
 
+const ERROR_MSG_DURATION = 3;
+const app = dva({
+    history:browserHistory,
+    onError(e){
+        message.error(e.message,ERROR_MSG_DURATION);
+    }
+
+});
+app.use(createLoading());
 app.model(require("./models/users"));
 
-// 2. Plugins
-// app.use({});
 
-// 3. Model
-// app.model(require('./models/example'));
-
-// 4. Router
 app.router(require('./router'));
 
-// 5. Start
 app.start('#root');
